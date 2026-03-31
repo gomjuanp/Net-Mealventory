bddddef407f906f4dacf5d5826787aea366e32c3
Juan Pablo Ordonez Gomez <165822202+gomjuanp@users.noreply.github.com>
2026-03-31 12:32:02 -0400

    Refactor code structure for improved readability and maintainability

--- Mealventory/Mealventory.Tests/FoodRepositoryTests.cs
+++ Mealventory/Mealventory.Tests/FoodRepositoryTests.cs
@@ -1,5 +1,6 @@
 using Mealventory.API.Repositories;
 using Mealventory.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mealventory.Tests;
 
@@ -9,7 +10,12 @@ public class FoodRepositoryTests
     [TestMethod]
     public void AddFoodItem_ShouldIncreaseCount()
     {
-        var repo = new FoodRepository();
+        var options = new Microsoft.EntityFrameworkCore.DbContextOptionsBuilder<Mealventory.API.Database.MealventoryDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb")
+            .Options;
+
+        using var context = new Mealventory.API.Database.MealventoryDbContext(options);
+        var repo = new FoodRepository(context);
 
         repo.Add(new FoodItem { Id = 1, Name = "Apple" });
 
